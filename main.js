f0df3ee77f104ae0f7a6ed454d53bf4a9adba6d7
Shivaani12345 <77334932+Shivaani12345@users.noreply.github.com>
2021-04-08 20:09:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+            var first_name = "Shivaani Shristi ";
+            var last_name = "Lahkar";
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 12464563636875845463654876465675346273464283642397461238;
+            var b = 37362416247234287364698768762173642736428371678;
+
+            var add = a + b ;
+            console.log(add);
+
+            var sub = a - b ;
+            console.log(sub);
+
+            var multiple = a * b ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
